c41a37c0cc2a541105ab5514cb78fe6864545108
NopeThisIsShini <getin2santa@gmail.com>
2024-03-06 10:41:00 +0530

    all about loops in jsq

---
+++ loops.js
@@ -0,0 +1,28 @@
+console.log("I am the tutorial on loops")
+let a =1;
+for (let i = 0; i < 5; i++) {
+    console.log(a+i);
+    
+}
+let obj ={
+    name: "Shini",
+    role: "Programmer",
+    company: "NopeThisIsShini"
+}
+for (const key in obj){ // if we want to print key of object we use for-in loop
+    const element = obj[key];
+    console.log(key,element);
+}
+for ( const c of "Shini"){ // for-of loop is used for iterable data-structure like array and string
+    console.log(c)
+}
+let b=0;
+while(b<6){ // while loop
+    console.log(b);
+    b++;
+}
+let i =10;
+do{
+    console.log(i)
+    i++;
+}while(i<4);

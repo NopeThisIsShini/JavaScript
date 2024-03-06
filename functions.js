a98b477d0f075168aa6c7502e23551e0911aa1c4
NopeThisIsShini <getin2santa@gmail.com>
2024-03-06 11:37:07 +0530

    functions in js

---
+++ functions.js
@@ -0,0 +1,19 @@
+function nice(name) {
+    console.log("Hey "+name + " You are nice")
+    console.log("Hey "+name+" you are good")
+}
+nice("Rohan")
+nice ("Shivam")
+
+function sum(a,b) {
+    return a+b;
+}
+result =sum (3,6)
+console.log(result)
+
+//arrow function
+const func1 = (x)=>{
+    console.log("I am an arrow function",x)
+}
+func1(34);
+func1(45);
